280a938ade91d35dcb6862cb4934329a3427ed0f
WOLF <blackwolf3466@gmail.com>
2025-06-09 05:30:22 +0300

    Done with sidebar initial look

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
-import { VioletButton } from "@/components/custom/violet-button";
+import { VioletButton } from "@/components/custom/violet-button"
 
 export default function Page() {
   return (
       <>
-        <VioletButton variant="outlined">
-          Test
-        </VioletButton>
+          <section className={"w-full h-screen flex flex-col items-center justify-center"}>
+              <h1>
+                  Welcome to Quizify
+              </h1>
+              <p>
+                  Your AI powered ✨ quiz generator application.
+              </p>
+              <VioletButton variant={"threed"} size={"lg"}>
+                  Get Started
+              </VioletButton>
+          </section>
       </>
     )
 }
---
+++ components/sidebar/app-sidebar.tsx
@@ -0,0 +1,173 @@
+"use client"
+
+import * as React from "react"
+import {
+    AudioWaveform,
+    BookOpen,
+    Bot,
+    Command,
+    Frame,
+    GalleryVerticalEnd,
+    Map,
+    PieChart,
+    Settings2,
+    SquareTerminal,
+    FileText
+} from "lucide-react"
+
+import {NavMain} from '@/components/sidebar/nav-main'
+import {NavProjects} from '@/components/sidebar/nav-projects'
+import {NavUser} from '@/components/sidebar/nav-user'
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarHeader,
+    SidebarRail,
+    SidebarMenu,
+    SidebarMenuItem,
+    SidebarMenuButton,
+} from '@/components/ui/sidebar'
+
+// This is sample data.
+const data = {
+    user: {
+        name: "shadcn",
+        email: "m@example.com",
+        avatar: "/avatars/shadcn.jpg",
+    },
+    navMain: [
+        {
+            title: "Quiz Manager",
+            url: "#",
+            icon: SquareTerminal,
+            isActive: true,
+            items: [
+                {
+                    title: "Create Quiz",
+                    url: "#",
+                },
+                {
+                    title: "View Quizes",
+                    url: "#",
+                },
+                {
+                    title: "History",
+                    url: "#",
+                },
+            ],
+        },
+        {
+            title: "Models",
+            url: "#",
+            icon: Bot,
+            items: [
+                {
+                    title: "Genesis",
+                    url: "#",
+                },
+                {
+                    title: "Explorer",
+                    url: "#",
+                },
+                {
+                    title: "Quantum",
+                    url: "#",
+                },
+            ],
+        },
+        {
+            title: "Documentation",
+            url: "#",
+            icon: BookOpen,
+            items: [
+                {
+                    title: "Introduction",
+                    url: "#",
+                },
+                {
+                    title: "Get Started",
+                    url: "#",
+                },
+                {
+                    title: "Tutorials",
+                    url: "#",
+                },
+                {
+                    title: "Changelog",
+                    url: "#",
+                },
+            ],
+        },
+        {
+            title: "Settings",
+            url: "#",
+            icon: Settings2,
+            items: [
+                {
+                    title: "General",
+                    url: "#",
+                },
+                {
+                    title: "Team",
+                    url: "#",
+                },
+                {
+                    title: "Billing",
+                    url: "#",
+                },
+                {
+                    title: "Limits",
+                    url: "#",
+                },
+            ],
+        },
+    ],
+    projects: [
+        {
+            name: "Design Engineering",
+            url: "#",
+            icon: Frame,
+        },
+        {
+            name: "Sales & Marketing",
+            url: "#",
+            icon: PieChart,
+        },
+        {
+            name: "Travel",
+            url: "#",
+            icon: Map,
+        },
+    ],
+}
+
+export function AppSidebar({...props}: React.ComponentProps<typeof Sidebar>) {
+    return (
+        <Sidebar collapsible="icon" {...props}>
+            <SidebarHeader>
+                <SidebarMenu>
+                    <SidebarMenuItem>
+                        <SidebarMenuButton
+                            asChild
+                            className="data-[slot=sidebar-menu-button]:!p-1.5"
+                        >
+                            <a href="#">
+                                <FileText className="!size-5"/>
+                                <span className="text-base font-semibold">Quizify</span>
+                            </a>
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarHeader>
+            <SidebarContent>
+                <NavMain items={data.navMain}/>
+                <NavProjects projects={data.projects}/>
+            </SidebarContent>
+            <SidebarFooter>
+                <NavUser user={data.user}/>
+            </SidebarFooter>
+            <SidebarRail/>
+        </Sidebar>
+    )
+}
